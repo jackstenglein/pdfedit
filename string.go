54f7df29bb0d1d15ac7875e04c4dd4b935ce07d0
Jack Stenglein <jackstenglein@utexas.edu>
2020-07-04 00:43:59 -0500

    Add basic implementation of string.go

--- string.go
+++ string.go
@@ -1,7 +1,12 @@
 package pdfedit
 
 // String represents a PDF string object.
-type String struct{}
+type String struct {
+	objectNumber     int
+	generationNumber int
+	value            string
+	isHexadecimal    bool
+}
 
 // Type returns the type of PDF object represented by the String. This is always the string
 // `String`. Type never returns an error. Type is implemented as part of the Object interface.
@@ -11,44 +16,59 @@ func (s *String) Type() (string, error) {
 
 // IsIndirect returns true if the String is an indirect object.
 func (s *String) IsIndirect() (bool, error) {
-	return false, nil
+	return s != nil && s.objectNumber > 0, nil
 }
 
 // ObjectNumber returns the object number for the String if it is an indirect object and -1 if it
-// is a direct object.
+// is a direct object. If the String is nil, 0 is returned.
 func (s *String) ObjectNumber() (int, error) {
-	return -1, nil
+	if s == nil {
+		return 0, nil
+	}
+	return s.objectNumber, nil
 }
 
 // GenerationNumber returns the generation number for the String if it is an indirect object and
-// -1 if it is a direct object.
+// -1 if it is a direct object. If the String is nil, -1 is returned.
 func (s *String) GenerationNumber() (int, error) {
-	return -1, nil
+	if s == nil {
+		return -1, nil
+	}
+	return s.generationNumber, nil
 }
 
-// Children always returns a nil list because PDF strings cannot have children. Children never
-// returns an error.
+// Children always returns a nil list because PDF strings cannot have children.
 func (s *String) Children() ([]*Object, error) {
 	return nil, nil
 }
 
 // Value returns the string value of the PDF String object.
-func (s *String) Value() (string, error) {
-	return "", nil
+func (s *String) Value() string {
+	if s == nil {
+		return ""
+	}
+	return s.value
 }
 
 // SetValue sets the String object to contain the provided value.
-func (s *String) SetValue(value string) error {
-	return nil
+func (s *String) SetValue(value string) {
+	if s != nil {
+		s.value = value
+	}
 }
 
 // IsHexadecimal returns true if the String object is represented in hexadecimal form in the PDF.
-func (s *String) IsHexadecimal() (bool, error) {
-	return false, nil
+func (s *String) IsHexadecimal() bool {
+	if s == nil {
+		return false
+	}
+	return s.isHexadecimal
 }
 
 // SetIsHexadecimal sets whether the String object will be represented in hexadecimal form when the
 // PDF file is saved.
-func (s *String) SetIsHexadecimal(isHexadecimal bool) error {
-	return nil
+func (s *String) SetIsHexadecimal(isHexadecimal bool) {
+	if s != nil {
+		s.isHexadecimal = isHexadecimal
+	}
 }
